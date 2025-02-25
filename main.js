d75e0c0456ce1d92cf14ecf0fdedbd971cc70c03
mahdiyeh-abiyar <mahdiehabyari@gmail.com>
2025-02-26 00:20:18 +0330

    Fix resume download issue

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ ELE.classList.add(localStorage.getItem("theme"));
 function downloadResume() {
   const link = document.createElement('a');
   link.style.display = 'none';
-  link.href = './assets/CV.pdf';
+  link.href = '/CV.pdf';
   link.setAttribute('download', 'Mahdiyeh_Abyiar_Resume.pdf'); 
   document.body.appendChild(link);
   link.click();
